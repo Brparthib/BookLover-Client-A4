1197d7e641806900f5b39d5532e3c2cb30587751
ParthibBr <bparthib77@gmail.com>
2025-07-06 19:21:06 +0600

    production link added as baseApi

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://booklover-server-a4.vercel.app/api",
+  }),
   tagTypes: ["book", "borrowed"],
   endpoints: (builder) => ({
     getAllBook: builder.query({
